302a95319bd2578cc91ff477d39141a6ea2da901
Your Name <you@example.com>
2024-10-15 12:34:45 +0530

    models done

---
+++ Backend/models/Turf.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const TurfSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  availability: [{ date: String, time: String }],
+});
+
+module.exports = mongoose.model('Turf', TurfSchema);
